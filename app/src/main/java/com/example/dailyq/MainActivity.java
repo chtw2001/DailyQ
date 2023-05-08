9a0356879918cf6e80524e15c783004efb6e08f6
chtw2001 <jungtaekwon1019@gmail.com>
2023-05-08 23:37:08 +0900

    changed

--- app/src/main/java/com/example/dailyq/MainActivity.java
+++ app/src/main/java/com/example/dailyq/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        dfsdsadf
+//        dfsdsad111111232asdfdsaf13123f
         //ad
     }
 }
