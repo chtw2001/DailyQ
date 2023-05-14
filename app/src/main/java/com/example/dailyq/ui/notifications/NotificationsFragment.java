c22be8f61dbdfcdbab6665e8de2f45b49fdd5ea7
un-known0 <nam0221@naver.com>
2023-05-15 01:45:34 +0900

    알림탭 대강 틀만 구현

--- app/src/main/java/com/example/dailyq/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/dailyq/ui/notifications/NotificationsFragment.java
@@ -1,37 +1,117 @@
 package com.example.dailyq.ui.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.dailyq.R;
 import com.example.dailyq.databinding.FragmentNotificationsBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private ListView listViewNotifications;
+    private List<String> notificationItems;
+    private CustomAdapter notificationItemsAdapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+    private void initialize(View view){
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        listViewNotifications = view.findViewById(R.id.listview_notifications);
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        notificationItems = new ArrayList<>();
+        notificationItems.add("Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1Notification 1");
+        notificationItems.add("Notification 2");
+        notificationItems.add("Notification 3");
+        notificationItemsAdapter = new CustomAdapter(getContext(),notificationItems);
+        listViewNotifications.setAdapter(notificationItemsAdapter);
+
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        initialize(view);
+        return view;
     }
+
+    protected class CustomAdapter extends BaseAdapter {
+        private Context mContext;
+        private List<String> mItems;
+
+        public CustomAdapter(Context context, List<String> items) {
+            mContext = context;
+            mItems = items;
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            TextView textView;
+            ImageView deleteButton;
+
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.layout_notification_item, null);
+            textView = (TextView) view.findViewById(R.id.textview_notification_item);
+            deleteButton = (ImageView) view.findViewById(R.id.delete_notification_item);
+
+
+            final String item = mItems.get(position);
+            textView.setText(item);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //클릭시 어디로 갈지 적기
+                    Toast.makeText(getContext(),item+"을 누름",Toast.LENGTH_SHORT).show();
+                }
+            });
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mItems.remove(item);
+                    notifyDataSetChanged();
+                }
+            });
+
+            return view;
+        }
+
+    }
+
+
+
+
 }
