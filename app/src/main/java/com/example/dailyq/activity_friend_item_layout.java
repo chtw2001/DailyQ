1c893eb5afc1cfd4ff73770e6fd176f89bf800f0
sunha12 <sunha9620@naver.com>
2023-05-15 02:57:59 +0900

    친구 리스트 추가. *ArrayList라서 앱 재실행하면 데이터 지워짐.

---
+++ app/src/main/java/com/example/dailyq/activity_friend_item_layout.java
@@ -0,0 +1,14 @@
+package com.example.dailyq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_friend_item_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_item_layout);
+    }
+}
